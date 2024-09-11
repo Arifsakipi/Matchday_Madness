7d00090e182fa077b93348457765f3da0716535d
Arifsakipi <arifsakipii@gmail.com>
2024-09-11 22:28:59 +0200

    added-recent matches to home page

--- MatchdayMadness2/Controllers/HomeController.cs
+++ MatchdayMadness2/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using MatchdayMadness2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MatchdayMadness2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DB _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DB db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            // Fetch the latest matches to display on the homepage
+            var matches = _db.Matches
+                .Include(m => m.HomeTeam)
+                .Include(m => m.AwayTeam)
+                .OrderByDescending(m => m.Date)
+                .Take(5) // Display only the 5 most recent matches
+                .ToList();
+
+            return View(matches);
         }
 
         public IActionResult Privacy()
